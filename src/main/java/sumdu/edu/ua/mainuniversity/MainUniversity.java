4944a27d4ac523c68c4f8ba146b61847bbbe5529
33ZoyaTaranets <vovkovna33@gmail.com>
2022-12-08 22:05:42 +0200

    Added

---
+++ src/main/java/sumdu/edu/ua/mainuniversity/MainUniversity.java
@@ -0,0 +1,9 @@
+package sumdu.edu.ua.mainuniversity;
+
+public class MainUniversity {
+
+    public static void main(String[] args) {
+        Student student1=new Student("Taras", "Ivanenko");
+        System.out.println(student1.toSting());
+    }
+}
