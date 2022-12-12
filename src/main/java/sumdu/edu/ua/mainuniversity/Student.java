8b65c4655a918a6bfc496a8f391ae9a386f21fea
33ZoyaTaranets <vovkovna33@gmail.com>
2022-12-12 17:51:18 +0200

    Added new class

--- src/main/java/sumdu/edu/ua/mainuniversity/Student.java
+++ src/main/java/sumdu/edu/ua/mainuniversity/Student.java
@@ -6,9 +6,45 @@ private static int nextId=1;
 private String name;
 private String surname;
 
-    public Student(int id, String name, String surname) {
-        this.id = id;
+    public Student(String name, String surname) {
+        this.id = nextId++;
         this.name = name;
+        this.surname = surname;
+    }
+
+    public String toSting(){
+        return this.id+" "+this.name+" "+this.surname;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Student.nextId = nextId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
         this.surname = surname;
     }
 
