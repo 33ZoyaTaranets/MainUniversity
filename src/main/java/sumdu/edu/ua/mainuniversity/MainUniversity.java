11a9a8009aedb1de872a2d0df1d030a863472011
33ZoyaTaranets <vovkovna33@gmail.com>
2022-12-12 17:51:12 +0200

    Added new class

--- src/main/java/sumdu/edu/ua/mainuniversity/MainUniversity.java
+++ src/main/java/sumdu/edu/ua/mainuniversity/MainUniversity.java
@@ -3,7 +3,45 @@ package sumdu.edu.ua.mainuniversity;
 public class MainUniversity {
 
     public static void main(String[] args) {
-        Student student1=new Student("Taras", "Ivanenko");
-        System.out.println(student1.toSting());
+        University  univ=new University("Sumy State University","2, Rimsky-Korsakov Str., 40007 Sumy, Ukraine");
+        System.out.println(univ.toSting());
+        
+        Department dep1=new Department("ElIt Department");
+        System.out.println(dep1.toSting());
+        
+        
+        Student student11=new Student("Ivan", "Ivanenko");
+        dep1.addToDepartment(student11);
+        
+        Student student12=new Student("Taras","Nesterenko");
+        dep1.addToDepartment(student12);
+        
+        Student student13=new Student("Serhiy","Petrenko");  
+        dep1.addToDepartment(student13);
+        
+        for(Student st:dep1.getStudents()){
+            System.out.println(st.toSting());
+        }
+        
+        
+        
+        Department dep2=new Department("Ekonom Department");
+        System.out.println(dep2.toSting());
+        
+        
+        Student student21=new Student("Ignat", "Vlasenko");
+        dep2.addToDepartment(student21);
+        
+        Student student22=new Student("Olena","Smila");
+        dep2.addToDepartment(student22);
+        
+        Student student23=new Student("Larysa","Malyuk");  
+        dep2.addToDepartment(student23);
+        
+        for(Student st:dep2.getStudents()){
+            System.out.println(st.toSting());
+        }
+        
+        
     }
 }
