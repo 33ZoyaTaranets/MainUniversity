f3629df06c1d6b518a79b70a894e967fdf5e0dc5
33ZoyaTaranets <vovkovna33@gmail.com>
2022-12-14 13:59:07 +0200

    Modified

--- src/test/java/sumdu/edu/ua/mainuniversitytest/StudentTest.java
+++ src/test/java/sumdu/edu/ua/mainuniversitytest/StudentTest.java
@@ -1,32 +1,31 @@
 package sumdu.edu.ua.mainuniversitytest;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import sumdu.edu.ua.mainuniversity.Student;
-
+//звів бібліотеки до використання анотацій jUnit 4 (стабільна і простіша версія)
+//Assertions залишив від jupiter (jUnit 5) як більш гнучкий інстркмент перевірок
 public class StudentTest {
 Student ab=null;
-
-    @BeforeAll
-    @Test
+//переглянув документацію тестів і методи налаштування можна маркувати виключно як передвиконання, без тесту якщо треба
+    @Before
     public void annotInform(){
         ab=new Student();
     }
-    
-    /*@Test
-    public void toStringTest(){
-        Student a=new Student("Stas","Petrenko");
-        System.out.print("2 Stas Petrenko ?="+a.toSting());
-        assertEquals("2 Stas Petrenko",a.toSting());
-    }
-    
     @Test
     public void getNameTest(){
         assertNull(ab.getName());
-    }*/
+    }
+    @Test
+    public void toStringTest(){
+        Student a=new Student("Stas","Petrenko");
+        System.out.print("2 Stas Petrenko ?="+a.toSting());
+        assertNotEquals("2 Stas Petrenko",a.toSting());
+//id студента 1, то результат буде "1 Стас..." або чекати з assertEquals "1 Stas.." або навпаки чекати щось інше
+    }
+    
+    
     
 }
