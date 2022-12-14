ed99a2dafe9559a5de6b0b16439e3cf396fe7c90
33ZoyaTaranets <vovkovna33@gmail.com>
2022-12-14 14:06:13 +0200

    Modified

--- src/test/java/sumdu/edu/ua/mainuniversitytest/DepartmentTest.java
+++ src/test/java/sumdu/edu/ua/mainuniversitytest/DepartmentTest.java
@@ -3,7 +3,7 @@ package sumdu.edu.ua.mainuniversitytest;
 import static junit.framework.Assert.assertEquals;
 import sumdu.edu.ua.mainuniversity.Department;
 import org.junit.Test;
-//замінив бібліотеку на org.junit.Test;
+
 public class DepartmentTest {
     
     @Test
