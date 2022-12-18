45b0377c51e2051ff3f0d77d1b1703d39231d741
33ZoyaTaranets <vovkovna33@gmail.com>
2022-12-18 21:55:10 +0200

    Modified

--- src/test/java/sumdu/edu/ua/mainuniversitytest/StudentTest.java
+++ src/test/java/sumdu/edu/ua/mainuniversitytest/StudentTest.java
@@ -1,5 +1,6 @@
 package sumdu.edu.ua.mainuniversitytest;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 import org.junit.Test;
 import org.junit.Before;
@@ -19,9 +20,9 @@ Student ab=null;
     }
     @Test
     public void toStringTest(){
-        Student a=new Student("Stas","Petrenko");
+        Student a=new Student(2,"Stas","Petrenko");
         System.out.print("2 Stas Petrenko ?="+a.toSting());
-        assertNotEquals("2 Stas Petrenko",a.toSting());
+        assertEquals("2 Stas Petrenko",a.toSting());
     }
     
     
